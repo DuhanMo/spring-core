a22ef5c1593b771621aac5f3913bc74e1c32d452
Duhanmo <devduhanmo@gmail.com>
2021-08-13 09:59:14 +0900

    새로운 할인정책 추가, AppConfig 추가

---
+++ src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,24 @@
+package hello.core;
+
+import hello.core.config.AppConfig;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        AppConfig appConfig = new AppConfig();
+        OrderService orderService = appConfig.orderService();
+        MemberService memberService = appConfig.memberService();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+        Order order = orderService.createOrder(1L, "모니터", 20000);
+
+        System.out.println(order.toString());
+
+    }
+
+}
