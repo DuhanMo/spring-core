d88e0d604dd4effb10eb91aa5e4d7db48860b499
moduhan <devduhanmo@gmail.com>
2021-10-29 00:40:02 +0900

    add: Component와 ComponentScan의 이해

---
+++ src/test/java/hello/core/scan/filter/BeanA.java
@@ -0,0 +1,9 @@
+package hello.core.scan.filter;
+
+/**
+ * 컴포넌트 스캔 대상에 추가할 클래스
+ * 커스텀한 MyIncludeComponent 애노테이션을 이용
+ */
+@MyIncludeComponent
+public class BeanA {
+}
---
+++ src/test/java/hello/core/scan/filter/BeanB.java
@@ -0,0 +1,9 @@
+package hello.core.scan.filter;
+
+/**
+ * 컴포넌트 스캔 대상에서 제외할 클래스
+ * 커스텀한 MyExcludeComponent 애노테이션을 이용
+ */
+@MyExcludeComponent
+public class BeanB {
+}
