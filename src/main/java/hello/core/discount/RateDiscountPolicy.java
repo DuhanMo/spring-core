16af72f6251369d695445fae6749566b797c3f8f
moduhan <devduhanmo@gmail.com>
2021-10-29 10:20:55 +0900

    add: @Qualifier, @Primary 이해

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,10 +1,14 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+//@Primary  Qualifier의 우선순위가 높지만 간결한 @Primary를 자주 사용한다.
 @Component
+@MainDiscountPolicy // Qualifier를 애노테이션으로만듦
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private final int discountRate = 10;
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,13 +1,13 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
@@ -15,12 +15,12 @@ public class OrderServiceImpl implements OrderService {
 
 //    생성자가 딱 1개만 있으면 @Autowired 생략이 가능. 물론 스프링 빈에만 해당한다.
 //    이마저도 롬복으로 지울 수 있다.
-//    @Autowired
-//    public OrderServiceImpl(MemberRepository memberRepository,
-//                            DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository,
+                            @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     /* setter에도 @Autowird로 주입이 가능하다.
     @Autowired
